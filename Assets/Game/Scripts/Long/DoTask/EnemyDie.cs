5c791330b8116e8452bf820ebeace78f07a9d95e
NHLong <kingboylong2005@gmail.com>
2025-11-27 12:21:22 +0700

    Fix EnemySpawn, Add EnemyDie

---
+++ Assets/Game/Scripts/Long/DoTask/EnemyDie.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyDie : MonoBehaviour
+{
+    public float lifeTime = 5f;
+
+    void Awake()
+    {
+        Debug.Log($"Spawn tại {transform.position}");
+    }
+    void Start()
+    {
+        Die();
+    }
+    private void Die()
+    {
+        Destroy(gameObject, lifeTime);
+        Debug.Log($"Enemy chết! InstanceID = {gameObject.GetInstanceID()}");
+    }
+}
--- Assets/Game/Scripts/Long/DoTask/SpawnEnemy.cs
+++ Assets/Game/Scripts/Long/DoTask/SpawnEnemy.cs
@@ -1,36 +1,55 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class SpawnEnemy : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    public static SpawnEnemy Instance;
+    public Transform player;
     public GameObject enemyPrefab;
-    public Transform[] spawnPoints;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        if (Instance == null) Instance = this;
-    }   
 
-    // Update is called once per frame
+    public float minSpawnDistance = 10f;
+    public float maxSpawnDistance = 20f;
+
+    public bool ActiveByButton = true;
+    public LayerMask groundMask;
+
+    public float spawnInterval = 2f;
+    private float timer;
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.P))
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval && !ActiveByButton)
         {
-            SpawnEnemies();
+            timer = 0f;
+            SpawnEnemy();
+        }
+        else if (ActiveByButton && Input.GetKeyDown(KeyCode.P))
+        {
+            SpawnEnemy();
         }
     }
-    private void SpawnEnemies()
+    public void SpawnEnemy()
     {
+        for (int i = 0; i < 20; i++) // thử tối đa 20 lần
+        {
+            Vector2 dir = Random.insideUnitCircle.normalized;
+            float distance = Random.Range(minSpawnDistance, maxSpawnDistance);
 
-        int index = Random.Range(0, spawnPoints.Length);
-        Vector3 spawnPos = spawnPoints[index].position;
+            // Random vị trí XZ quanh player
+            Vector3 spawnXZ = player.position + new Vector3(dir.x, 0f, dir.y) * distance;
 
-        Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
-        
+            // Raycast từ trên cao xuống
+            Vector3 rayStart = spawnXZ + Vector3.up * 100f;
+
+            if (Physics.Raycast(rayStart, Vector3.down, out RaycastHit hit, 200f, groundMask))
+            {
+                // hit.point là vị trí mặt đất
+                Instantiate(enemyPrefab, hit.point, Quaternion.identity);
+                return;
+            }
+        }
+
+        Debug.Log("Không tìm được vị trí spawn hợp lệ!");
     }
-    // public Vector3 GetRandomSpawnPoint()
-    // {
-    //     int index = Random.Range(0, spawnPoints.Length);
-    //     return spawnPoints[index].position;
-    // }
 }
